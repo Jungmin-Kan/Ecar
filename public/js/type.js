c46b3f82b3ee9efc85810b3df7a50467e048dbf0
kjmTJ <51221534+kjmTJ@users.noreply.github.com>
2020-01-22 21:34:19 +0900

    Update type.js

--- public/js/type.js
+++ public/js/type.js
@@ -14,9 +14,8 @@ var getHtml = async() => {
 getHtml()
     .then(html => {
         let ulList = [];
-        const $ = cheerio.load(html.data); //html 문자열을 받아 cheerio 객체를 반환합니다.
+        const $ = cheerio.load(html.data);
         const $bodyList = $("table.table_02_2_1 tbody").eq(0).children("tr");
-        // 태그.class 안에 ul태그  안에  li태그.class이름
         for (var row = 0; row < $bodyList.length; row++) {
             var cells = $bodyList.eq(row).children();
             var cols = [];
@@ -30,7 +29,4 @@ getHtml()
             ulList.push(cols);
         }
         return array;
-}).then(/*res=> log(res)*/); // return된 data는 res가 된다
-
-// http://magic.wickedmiso.com/142
-//   https://velog.io/@yesdoing/Node.js-%EC%97%90%EC%84%9C-%EC%9B%B9-%ED%81%AC%EB%A1%A4%EB%A7%81%ED%95%98%EA%B8%B0-wtjugync1m
+}).then(res=> log(res));
